dcdb0e8df6ca491a8ac64015d58de5a020223267
JaimeJ177 <jxa.omega@gmail.com>
2020-01-17 12:08:20 -0600

    cart get

--- homework9/src/app/modules/store/components/cart/cart.component.ts
+++ homework9/src/app/modules/store/components/cart/cart.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { IAppState } from 'src/app/store/state/app.state';
+import { CartItems } from '../../../shared/interfaces/cart-items.interface';
+import { getCartItems } from 'src/app/store/selectors/store.selectors';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent implements OnInit {
+export class CartComponent implements OnInit, OnDestroy {
+  cartList: CartItems[];
 
   constructor(private store: Store<IAppState>) { }
 
   ngOnInit() {
-
+    this.store.select(getCartItems).subscribe(
+      data => {
+        this.cartList = data;
+      }
+    );
   }
 
+  ngOnDestroy() {
+  }
 }
