2dd7bd27bb0e3268cb0134ec60b8c2013605c6c7
JaimeJ177 <jxa.omega@gmail.com>
2020-01-17 14:47:04 -0600

    function changed

--- homework9/src/app/modules/store/components/product-card/product-card.component.ts
+++ homework9/src/app/modules/store/components/product-card/product-card.component.ts
@@ -1,10 +1,16 @@
-import { getProducts, getLoginToken } from './../../../../store/selectors/store.selectors';
+import {
+  getProducts,
+  getLoginToken
+} from './../../../../store/selectors/store.selectors';
 import { Product } from './../../../shared/interfaces/product.interface';
 import { Component, OnInit } from '@angular/core';
 import { IAppState } from 'src/app/store/state/app.state';
 import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
-import { GetProductAction, LikeProductAction } from '../../../../store/actions/product.actions';
+import {
+  GetProductAction,
+  LikeProductAction
+} from '../../../../store/actions/product.actions';
 import { Observable } from 'rxjs';
 import { AddCartAction } from '../../../../store/actions/cart.action';
 
@@ -17,11 +23,12 @@ export class ProductCardComponent implements OnInit {
   products: Product[];
   canLike$: Observable<boolean> = this.store.select(getLoginToken);
 
-  constructor(private store: Store<IAppState>,  private router: Router) { }
+  constructor(private store: Store<IAppState>, private router: Router) {}
 
   ngOnInit() {
-    this.store.select(getProducts)
-      .subscribe(productList =>  this.products = productList);
+    this.store
+      .select(getProducts)
+      .subscribe(productList => (this.products = productList));
   }
 
   openProduct(id: number) {
@@ -29,9 +36,24 @@ export class ProductCardComponent implements OnInit {
     this.router.navigate(['product']);
   }
 
-  addCart(id: number, amount: number) {
+  addCart(
+    id: number,
+    amount: number,
+    name: string,
+    url: string,
+    price: string
+  ) {
     const cartId = new Date();
-    this.store.dispatch(new AddCartAction({id: cartId.getTime(), product_variant_id: id, quantity: amount}));
+    this.store.dispatch(
+      new AddCartAction({
+        id: cartId.getTime(),
+        product_variant_id: id,
+        quantity: amount,
+        price: price,
+        url: url,
+        name: name
+      })
+    );
   }
 
   likeProduct(id: number, like: number) {
