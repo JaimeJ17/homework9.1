dd99986fdc158761dc516ad739dd932d6921d529
JaimeJ177 <jxa.omega@gmail.com>
2020-01-16 18:40:58 -0600

    firebase made

--- homework9/src/environments/environment.ts
+++ homework9/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCs089oqosp5BcS2FEW_qGB0a-w_J8yNog",
+    authDomain: "week9-70f78.firebaseapp.com",
+    databaseURL: "https://week9-70f78.firebaseio.com",
+    projectId: "week9-70f78",
+    storageBucket: "week9-70f78.appspot.com",
+    messagingSenderId: "325846266003",
+    appId: "1:325846266003:web:94ca5e3d62cf3fbf141d66",
+    measurementId: "G-DCED76RYSQ"
+  }
 };
 
 /*
