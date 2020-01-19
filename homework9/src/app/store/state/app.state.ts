98110d3e8afa6c5ddda54da12951aad72d7abe79
Jaime Alejandro <jxa.omega@gmail.com>
2020-01-19 04:44:08 -0600

    new properties

--- homework9/src/app/store/state/app.state.ts
+++ homework9/src/app/store/state/app.state.ts
@@ -1,7 +1,16 @@
+import { LoginState } from './../reducers/my-Login.reducer';
+import { CartState } from './../reducers/my-Cart.reducer';
+import { ProductState } from './../reducers/my-Product.reducer';
+import { CategoryState } from './../reducers/my-Category.reducer';
 import { IProductState, initialProductSate } from './product.state';
 
 export interface IAppState {
   myStore: IProductState;
+  category?: CategoryState;
+  product?: ProductState;
+  cart?: CartState;
+  login?: LoginState;
+
 }
 
 export const initialAppState: IAppState = {
