df858e7d88a85f5ad7e4a39c9559df20671654c7
Jaime Alejandro <jxa.omega@gmail.com>
2020-01-19 04:42:40 -0600

    new reducers

--- homework9/src/app/store/reducers/index.ts
+++ homework9/src/app/store/reducers/index.ts
@@ -1,9 +1,15 @@
 import {ActionReducerMap} from '@ngrx/store';
-
 import { productReducer } from './product.reducer';
-import { IAppState } from '../state/app.state';
+import { categoryReducer } from './my-Category.reducer';
+import { productsReducer } from './my-Product.reducer';
+import { cartReducer } from './my-Cart.reducer';
+import { loginReducer } from './my-Login.reducer';
 
-export const reducers: ActionReducerMap<IAppState, any> = {
+export const reducers: ActionReducerMap<any> = {
   myStore: productReducer,
+  category: categoryReducer,
+  product: productsReducer,
+  cart: cartReducer,
+  login: loginReducer
 };
 
