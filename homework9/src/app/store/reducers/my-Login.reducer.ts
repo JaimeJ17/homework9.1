4793912dea6f167c6ec6de201a726c4b586a096e
Jaime Alejandro <jxa.omega@gmail.com>
2020-01-19 04:43:08 -0600

    new login reducer

---
+++ homework9/src/app/store/reducers/my-Login.reducer.ts
@@ -0,0 +1,51 @@
+import { loginInitialState } from './../state/constants/login-initialState.constant';
+import { Login } from './../../modules/shared/interfaces/login.interface';
+import { LocalstorageService } from 'src/app/modules/shared/services/localstorage.service';
+import { LoginActions, ELoginActions } from '../actions/login.action';
+import { userState } from '../state/constants/user-initialState.constant';
+import { tokenState } from '../state/constants/token-initialState.constant';
+const storage: LocalstorageService = new LocalstorageService();
+
+
+export interface LoginState {
+  login: Login;
+}
+
+
+const defualtLogin: LoginState = {
+  login: loginInitialState
+};
+
+
+
+export function loginReducer(
+  state: LoginState = defualtLogin,
+  action: LoginActions
+): LoginState {
+  switch (action.type) {
+    case ELoginActions.GetLoginsSuccess: {
+      return {
+        ...state,
+        login: action.payload
+      };
+    }
+    case ELoginActions.GetLoginsFailure: {
+      storage.removefile('user');
+      storage.removefile('token');
+      return {
+        ...state,
+        login: { user: userState, token: tokenState }
+      };
+    }
+    case ELoginActions.LogOut: {
+      storage.removefile('user');
+      storage.removefile('token');
+      return {
+        ...state,
+        login: { user: userState, token: tokenState }
+      };
+    }
+    default:
+      return { ...state };
+  }
+}
