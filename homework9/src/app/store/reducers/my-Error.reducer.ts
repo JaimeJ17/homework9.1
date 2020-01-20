5e37e93964e385729924d2a70e9cdeb8460261fa
Jaime Alejandro <jxa.omega@gmail.com>
2020-01-19 21:28:55 -0600

    new reducer

---
+++ homework9/src/app/store/reducers/my-Error.reducer.ts
@@ -0,0 +1,25 @@
+import { EErrorActions, GetErrorAction } from './../actions/error.actions';
+import { Error } from './../../modules/shared/interfaces/error.interface';
+
+export interface ErrorState  {
+  error: Error;
+}
+
+const defaultError: ErrorState = {
+  error: {
+    errors : []
+  }
+};
+
+
+export function errorReducer(state: ErrorState = defaultError, action: GetErrorAction): ErrorState{
+  switch (action.type) {
+    case EErrorActions.GetError: {
+      return { ...state, error: action.payload };
+    }
+
+    default: {
+      return {... state};
+    }
+  }
+}
