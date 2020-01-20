c742e62b8a1536ec4143c8c4e3f139eb5d0b56ba
Jaime Alejandro <jxa.omega@gmail.com>
2020-01-19 21:29:26 -0600

    product reducer modified

--- homework9/src/app/store/reducers/my-Product.reducer.ts
+++ homework9/src/app/store/reducers/my-Product.reducer.ts
@@ -1,20 +1,26 @@
+import { state } from '@angular/animations';
 import { EProductActions } from './../actions/product.actions';
 import { Product } from './../../modules/shared/interfaces/product.interface';
 import { createEntityAdapter, EntityState } from '@ngrx/entity';
 import { createFeatureSelector } from '@ngrx/store';
 import { ProductActions } from '../actions/product.actions';
+import { ACTIONS_SUBJECT_PROVIDERS } from '@ngrx/store/src/actions_subject';
 
 
 export const productAdapter = createEntityAdapter<Product>();
 export interface ProductState extends EntityState<Product> {
   currentProduct: Product;
+  categoryFilter: string;
+  searchQuery: string;
 }
 
 const defualtProduct = {
   ids: [],
   entities: {
   },
-  currentProduct: null
+  currentProduct: null,
+  categoryFilter: null,
+  searchQuery: null
 };
 
 export const initialProductState: ProductState =  productAdapter.getInitialState(defualtProduct);
@@ -26,11 +32,44 @@ export function productsReducer(state: ProductState = initialProductState, actio
       return productAdapter.addMany(action.payload, state);
     }
 
+    case EProductActions.GetProductsPerCategorySuccess: {
+      return productAdapter.addMany(action.payload, state);
+    }
+
     case EProductActions.GetProduct: {
       return {
         ...state,
         currentProduct: state.entities[action.payload],
       };
+    }
+
+    case EProductActions.SaveFilterCategory: {
+      if (state.categoryFilter === action.payload) {
+        return {
+          ...state,
+          categoryFilter: null,
+        };
+      } else {
+        return {
+          ...state,
+          categoryFilter: action.payload,
+        };
+      }
+    }
+
+    case EProductActions.SaveFilterSearch: {
+      return {
+        ...state,
+        searchQuery: action.payload,
+      };
+    }
+
+    case EProductActions.SearchProductsSuccess: {
+      return productAdapter.addMany(action.payload, state);
+    }
+
+    case EProductActions.LikeProductSuccess: {
+      return { ...state };
     }
 
     default: {
