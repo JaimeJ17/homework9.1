91f0d58115cb3edef4af7c3138f49473edcf7d25
Jaime Alejandro <jxa.omega@gmail.com>
2020-01-23 23:50:55 -0600

    slide animation

--- homework9/src/app/modules/shared/constants/slide.constant.ts
+++ homework9/src/app/modules/shared/constants/slide.constant.ts
@@ -9,54 +9,54 @@ import {
 
 } from '@angular/animations';
 
-export const slideInAnimation =
-  trigger('routerAnimations', [
-    transition('login <=> home', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
+export const sliderAnimation = trigger('routerAnimations', [
+  transition('home <=> login', [
+    style({ position: 'relative' }),
+    query(':enter, :leave', [
+      style({
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%'
+      })
+    ], { optional: true}),
+    query(':enter', [
+      style({ left: '-100%' })
+    ], { optional: true}),
+    query(':leave', animateChild(), { optional: true}),
+    group([
+      query(':leave', [
+        animate('300ms ease-out', style({ left: '100%' }))
+      ], { optional: true}),
       query(':enter', [
-        style({ left: '-100%'})
-      ]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [
-          animate('300000ms ease-out', style({ left: '100%'}))
-        ]),
-        query(':enter', [
-          animate('300000ms ease-out', style({ left: '0%'}))
-        ])
-      ]),
-      query(':enter', animateChild()),
+        animate('300ms ease-out', style({ left: '0%' }))
+      ], { optional: true})
     ]),
-    transition('* <=> product', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ]),
+    query(':enter', animateChild(), { optional: true}),
+  ]),
+
+  transition('* <=> *', [
+    style({ position: 'relative' }),
+    query(':enter, :leave', [
+      style({
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%'
+      })
+    ], { optional: true}),
+    query(':enter', [
+      style({ left: '-100%' })
+    ], { optional: true}),
+    query(':leave', animateChild(), { optional: true}),
+    group([
+      query(':leave', [
+        animate('200ms ease-out', style({ left: '100%' }))
+      ], { optional: true}),
       query(':enter', [
-        style({ left: '-100%'})
-      ]),
-      query(':leave', animateChild()),
-      group([
-        query(':leave', [
-          animate('300000ms ease-out', style({ left: '100%'}))
-        ]),
-        query(':enter', [
-          animate('300000ms ease-out', style({ left: '0%'}))
-        ])
-      ]),
-      query(':enter', animateChild()),
-    ])
-  ]);
+        animate('300ms ease-out', style({ left: '0%' }))
+      ], { optional: true})
+    ]),
+    query(':enter', animateChild(), { optional: true}),
+  ])
+]);
